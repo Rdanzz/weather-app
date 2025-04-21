465d75700353e338d772cc77c641ce648d3fd24a
Rdanzz <zidhanraffly123@gmail.com>
2025-04-22 01:51:03 +0700

    footer copyright

--- src/components/layouts.tsx
+++ src/components/layouts.tsx
@@ -4,13 +4,16 @@ import Header from "./header";
 const Layouts = ({ children }: PropsWithChildren) => {
   return (
     <div className="bg-gradient-to-br from-background to-muted">
-      <Header/>
+      <Header />
       <main className="min-h-screen container mx-auto px-4 py-8">
         {children}
       </main>
-      <footer className="border-t backdrop-blur py-12 supports-[backdrop-filter]:bg-background/60">
+      <footer className="border-t backdrop-blur py-8 supports-[backdrop-filter]:bg-background/60">
         <div className="container mx-auto px-4 text-center text-gray-400">
-            <p>Copyright @rdanz 2025</p>
+          <p className="text-center text-sm text-muted-foreground">
+            &copy; {new Date().getFullYear()} WeatherApp by{" "}
+            <span className="font-semibold">ziidhan.rraffly_</span>. All rights reserved.
+          </p>
         </div>
       </footer>
     </div>
